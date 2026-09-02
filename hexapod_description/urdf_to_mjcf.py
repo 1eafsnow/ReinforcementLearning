c2a04593a4dcd1d6f830b537847691dd2346e978
1eafsnow <46445616+1eafsnow@users.noreply.github.com>
2026-09-02 09:45:27 +0800

    Delete hexapod_description/urdf_to_mjcf.py

--- hexapod_description/urdf_to_mjcf.py
+++
@@ -1,7 +0,0 @@
-import mujoco
-import os
-
-model = mujoco.MjModel.from_xml_path('./urdf/hexapod_model.urdf')
-mujoco.mj_saveLastXML("./mjcf/hexapod_model.xml", model)
-
-print("export to hexapod_model.xml")
